8971a15d97e839a1d4638d0f626d9eb65b55e749
gab5334 <gxb5334@psu.edu>
2026-09-16 10:07:47 -0400

    Define funcs

---
+++ Sched.py
@@ -0,0 +1,17 @@
+def parse_instruction(instruction: str) -> None:
+    """
+    Takes in whole instruction
+    Returns components
+    """
+    #R,<REG>,<REG>,<REG>
+    #I,<REG>,<REG>,<IMM>
+    #L,<REG>,<IMM>,<REG>
+    #S,<REG>,<IMM>,<REG>
+    pass
+
+def load_hazard() -> None:
+    """
+    register-carried load-use dependencies
+    For each instruction in the input file, produce a corresponding line in out.txt of the form
+    """
+    pass
